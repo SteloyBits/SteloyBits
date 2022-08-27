de71ef0b9df7487222f322e30e3c0fec5cef9168
Abdulsamad <feakaustin@gmail.com>
2022-08-27 02:26:21 +0100

    Refactored Code. Very neat and bug fixed

--- stemdisplay.py
+++ stemdisplay.py
@@ -1,47 +1,52 @@
-feed_data = input("Input the values of data you want to stem-display (seperate with single spacing):\n\n")
-feed_data = feed_data.split()
-fed_data = []
-for hin in feed_data:
-	hin = int(hin)
-	fed_data.append(hin)
+import time
 
-def unitdetect(x):
+
+#for 1 value
+user_input = input("Input the values of data you want to stem-display (seperate with single spacing):\n\n")
+feed_data = user_input.split()
+values = []
+for i in feed_data:
+	values.append(int(i))
+def one_digit(main_digit):
+	new_list = []
+	for i in str((main_digit)):
+		new_list.append(i)
+	final_list = new_list
+	if len(final_list) > 1:
+		power = 10 ** (len(final_list) - 1)
+	else:
+		power = 10 ** 1
+	return power
+
+def unitdetect(val, power):
 	'''
 	This function check to determine the place value of the
 	given number data. This will allow us to work faster as 
 	we go deeper in our main program.
 	'''
-	if (x/10) > 0 and (x/10) < 1:
-		stem = 0
-		leaf = x
-	elif (x/10) < 10:
-		stem, leaf = x//10, x%10
-	else:
-		fed_data.remove(x)
-		
-	
+	stem, leaf = val // one_digit(power),  val % one_digit(power)
 	return stem, leaf
+	
+	
 
 def stemdisplay(data_list):
-	'''
-	Create a dictionary. Loop through your data list; fill in your
-	dictionary and sort. Showcase the output.
-	'''
+
 	dictlist = dict()
+	val = list()
 	for number in data_list:
-		val = unitdetect(number)
+		k = one_digit(number)
+		val = unitdetect(number, k)
 		if val[0] not in dictlist:
 			dictlist[val[0]] = [val[1]]
 		else:
 			dictlist[val[0]].append(val[1])
-			dictlist[val[0]].sort()
 		y = list(dictlist.keys())
 		y.sort()
 		
-	print ("\nStem	  Leaf")			
+	print ("\n Stem	    Leaf")			
 	for g in y:
-		print (f"{g}	  {dictlist[g]}")
-		
-	
-stemdisplay(fed_data)
+		print (f"  {g}	    {dictlist[g]}")
 
+	
+stemdisplay(values)
+time.sleep(0.5)
